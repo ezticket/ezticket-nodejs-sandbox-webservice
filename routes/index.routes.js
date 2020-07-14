f2d261ac79cf2b13270b96f30896b78b370d7c3e
Pablo Guillén <pguillen@carrasco.uruit.com>
2020-07-13 23:53:01 -0300

    Revert "Configuración eslint"
    This reverts commit 4d9f5fe9668369b09743d8697ec07a3d9fa1dc6c.

--- routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET first route */
-router.get('/', function(req, res, next) {
-  res.json({ message: 'Hello world' });
-});
-
-module.exports = router;
