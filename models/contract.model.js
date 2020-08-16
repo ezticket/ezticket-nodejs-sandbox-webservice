6178b2d49f2f70b2691ede7b0763db4aabcbde60
lucasl <lucasl@oktana.io>
2020-08-16 19:19:52 -0300

    se agrega el codigo para vender un ticket con blockchain

--- models/contract.model.js
+++ models/contract.model.js
@@ -4,18 +4,17 @@ const Event = require("../build/contracts/Event.json");
 
 class Contract {
   constructor() {
-    const PRIVATE_KEY =
-      "0xa446fb3d32c9722973389759853f98534270249439ca5167e5822382fa0281ee";
+    const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
     if (!Contract.instance) {
       const provider = new HDWalletProvider(
         PRIVATE_KEY,
-        "https://ropsten.infura.io/v3/37570d6f7c744976ab77ea208c574bcf"
+        process.env.CONTRACT_NET
       );
       const web3 = new Web3(provider);
       Contract.instance = new web3.eth.Contract(
         Event.abi,
-        "0xb6446f5da4ab07d642df22a5a556e64a2f70e845"
+        process.env.CONTRACT_ADDRESS
       );
     }
   }
@@ -25,7 +24,7 @@ class Contract {
   }
 
   getAddress() {
-    return "0x3f9D9EFB08E314Eba444A231BDF321B711Fc32bc";
+    return process.env.CONTRACT_ADDRESS;
   }
 }
 
--- models/ticket.model.js
+++ models/ticket.model.js
@@ -1,26 +1,44 @@
-const helper = require('../helpers/helper.js');
+const helper = require("../helpers/helper.js");
+const eventContract = require("../models/contract.model");
+const contract = new eventContract().getInstance();
 
-function scan (ticketId, newScan) {
-    return new Promise((resolve, reject) => {
-        data = { ticketId, newScan };
-        resolve(data);
-    });
+function scan(ticketId, newScan) {
+  return new Promise((resolve, reject) => {
+    data = { ticketId, newScan };
+    resolve(data);
+  });
 }
 
-function sell (tickets) {
-    return new Promise((resolve, reject) => {
-        resolve(tickets);
-    });
+async function sell(tickets) {
+  console.log("Starting sell: ", tickets);
+  var ticketsIds = [];
+  var usersIds = [];
+  var purchasedAt = [];
+  var today = new Date().toISOString();
+  tickets.forEach(ticketInfo => {
+    ticketsIds.push(ticketInfo.ticketId);
+    usersIds.push(ticketInfo.buyerId);
+    purchasedAt.push(today);
+  });
+  const newSale = await contract.methods
+    .addTicketList(ticketsIds, usersIds, purchasedAt)
+    .send({ from: process.env.ADDRESS });
+  console.log(newSale);
+  console.log(newSale.transactionHash);
+
+  return new Promise((resolve, reject) => {
+    resolve(newSale.transactionHash);
+  });
 }
 
-function resell (tickets) {
-    return new Promise((resolve, reject) => {
-        resolve(tickets);
-    });
+function resell(tickets) {
+  return new Promise((resolve, reject) => {
+    resolve(tickets);
+  });
 }
 
 module.exports = {
-    scan,
-    sell,
-    resell
+  scan,
+  sell,
+  resell
 };
