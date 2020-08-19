06eb59c6abd3bc39fb921462ce17298e2db44ca6
PGuillen <pguillen@gmail.com>
2020-08-19 19:15:51 -0300

    Add editorconfig

--- utils/uEnvironment.js
+++ utils/uEnvironment.js
@@ -1,39 +1,39 @@
-const fs = require("fs");
-const { Table } = require("console-table-printer");
+const fs = require('fs');
+const { Table } = require('console-table-printer');
 
-function loadEnvironment() {
-  const NODE_ENV = process.env.NODE_ENV || "development";
+const loadEnvironment = () => {
+  const NODE_ENV = process.env.NODE_ENV || 'development';
 
-  console.log("Environment config file:", `'.env.${NODE_ENV}'`);
+  console.log('Environment config file:', `'.env.${NODE_ENV}'`);
 
   if (fs.existsSync(`.env.${NODE_ENV}`)) {
-    require("dotenv").config({
+    require('dotenv').config({
       path: `.env.${NODE_ENV}`
     });
 
-    console.info("Environment config file loadeed Ok");
+    console.info('Environment config file loadeed Ok');
   } else {
-    console.console.warn("Error loading environment file");
+    console.console.warn('Error loading environment file');
   }
 
   printEnvironment();
-}
+};
 
-printEnvironment = () => {
+const printEnvironment = () => {
   const environmentList = [
-    "ENV",
-    "ADDRESS",
-    "CONTRACT_ADDRESS",
-    "PRIVATE_KEY",
-    "CONTRACT_NET"
+    'ENV',
+    'ADDRESS',
+    'CONTRACT_ADDRESS',
+    'PRIVATE_KEY',
+    'CONTRACT_NET'
   ];
 
   try {
     const myTable = new Table({
       columns: [
-        { name: "index", alignment: "center" },
-        { name: "Const Name", alignment: "left" },
-        { name: "Value", alignment: "left" }
+        { name: 'index', alignment: 'center' },
+        { name: 'Const Name', alignment: 'left' },
+        { name: 'Value', alignment: 'left' }
       ]
     });
 
@@ -43,16 +43,16 @@ printEnvironment = () => {
       myTable.addRow(
         {
           index: index + 1,
-          "Const Name": element,
-          Value: process.env[element] ? process.env[element] : "LOST"
+          'Const Name': element,
+          Value: process.env[element] ? process.env[element] : 'LOST'
         },
-        { color: process.env[element] ? "green" : "red" }
+        { color: process.env[element] ? 'green' : 'red' }
       );
     }
 
     myTable.printTable();
   } catch (error) {
-    console.error("Error in printEnvironment", error);
+    console.error('Error in printEnvironment', error);
   }
 };
 
